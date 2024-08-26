fb29983484a83dfd9ffb0a42a5e233769d230d86
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-08-26 06:20:12 +0000

    240826

--- react-hooks-practice/src/App.tsx
+++ react-hooks-practice/src/App.tsx
@@ -1,6 +1,7 @@
-import { useContext, useEffect, useReducer, useRef, useState } from 'react';
+import { useCallback, useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 import './App.css';
 import UserInfoContext from './main';
+import SomeChild from './SomeChild'
 
 const reducer = (state, action) => {
   switch(action.type){
@@ -15,6 +16,29 @@ const reducer = (state, action) => {
 
 function App(){
   const [count, setCount] = useState(0); 
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  //useMemoは、メモリに情報（値）を保存することができる。
+  //プログラムから上から順に実行される。
+  //DOMが書き換えられたときも上から順に実行されるが、その書き換えに関係ない処理も実行される。
+  //間に重い処理が挟まっていると、その処理に関係ないDOM操作をした時も読み込みに時間がかかってしまう。
+  //関係あるDOM操作のときだけ処理を実行するように指定することができる。
+  //パフォーマンス改善のとき用いられる。
+  const square = useMemo(() => {
+    let i = 0;
+    while (i<200000000){
+      i++;
+    }
+    return count02 * count02;
+  }, [count02]);
+
+  //useCallBackは、useMemoの関数版。メモリに関数を保存することができる。
+  const [counter, setCounter] = useState(0);
+
+  const showCount = useCallback(() => {
+    alert('これは重い処理です。');
+  }, [counter]);
 
   //useContextは、異なるtsxファイル間で情報を共有できる。
   //ログイン情報の共有などに使用される。
@@ -64,6 +88,18 @@ function App(){
       <button onClick={() => dispatch({type: "increment"})}>+</button>
       <button onClick={() => dispatch({type: "decrement"})}>-</button>
 
+      <hr/>
+      <h1>useMemo</h1>
+      <p>カウント１：{count01}</p>
+      <p>カウント２：{count02}</p>
+      <p>結果：{square}</p>
+      <button onClick={() => setCount01(count01 + 1)}>+</button>
+      <button onClick={() => setCount02(count02 + 1)}>+</button>
+
+      <hr/>
+      <h1>useCallBack</h1>
+      <SomeChild showCount = { showCount }/>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
 
     </div>
   );
